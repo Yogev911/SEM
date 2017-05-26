a58a24b1c3b787740b2ae1417ceacbc4e14f37c1
Yogev Heskia <Yogev911@gmail.com>
2017-05-26 12:38:05 +0300

    .
    .

---
+++ Button/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "button main cpp\n";
+	system("Pause");
+	return 0;
+}
---
+++ ComboBox/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "ComboBox main cpp\n";
+	system("Pause");
+	return 0;
+}
