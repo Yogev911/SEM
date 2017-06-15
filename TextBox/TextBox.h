d392156fe45beee6f4f7359a0efae131e1dc1587
Yogev Heskia <Yogev911@gmail.com>
2017-06-15 14:39:46 +0300

    adding textbox

--- TextBox/TextBox.h
+++ TextBox/TextBox.h
@@ -2,11 +2,27 @@
 
 #include "../Control/Control.h"
 
-class TextBox
+class TextBox : public Control
 {
+	string value;
+
+	int curserPosition;
+	void moveRight();
+	void moveLeft();
+	void deleteRight();
+	void deleteLeft();
+	void deleteLast();
+	void addCharecter(CHAR c);
+
 public:
 	TextBox(int width);
 	void setValue(string value);
 	string getValue();
+	void draw(Graphics &graphics, int, int, size_t);
+	void keyDown(WORD, CHAR);
+	void mousePressed(short, short, bool);
+	int getCurserPosition();
+	void moveCurser(Graphics);
 	~TextBox();
 };
+
